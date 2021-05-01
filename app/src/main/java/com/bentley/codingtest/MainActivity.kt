d9b03b8494e96423d2b9dedb4b0e0d2c0c37d453
Bentley Park <junggil.park@sovoro.kr>
2021-05-01 12:11:54 +0900

    viewbiding 설정

--- app/src/main/java/com/bentley/codingtest/MainActivity.kt
+++ app/src/main/java/com/bentley/codingtest/MainActivity.kt
@@ -2,8 +2,14 @@ package com.bentley.codingtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bentley.codingtest.databinding.ActivityMainBinding
+import com.bentley.codingtest.utils.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/bentley/codingtest/utils/ExFuctions.kt
@@ -0,0 +1,102 @@
+package com.bentley.codingtest.utils
+
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+    crossinline bindingInflater: (LayoutInflater) -> T
+) = lazy(LazyThreadSafetyMode.NONE) {
+    bindingInflater.invoke(layoutInflater)
+}
+
+//// 액티비티 전환 후 종료하는 함수
+//inline fun <reified T : Activity> Activity.launchActivityWithFinish() {
+//    val intent = Intent(this, T::class.java)
+//    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+//    startActivity(intent)
+//    overridePendingTransition(0, 0)
+//    finish()
+//}
+//
+//fun <T> Fragment.viewLifecycle(): ReadWriteProperty<Fragment, T> =
+//    object : ReadWriteProperty<Fragment, T>, DefaultLifecycleObserver {
+//
+//        private var binding: T? = null
+//
+//        init {
+//            this@viewLifecycle
+//                .viewLifecycleOwnerLiveData
+//                .observe(this@viewLifecycle, { owner: LifecycleOwner? ->
+//                    owner?.lifecycle?.addObserver(this)
+//                })
+//        }
+//
+//        override fun onDestroy(owner: LifecycleOwner) {
+//            binding = null
+//        }
+//
+//        override fun getValue(
+//            thisRef: Fragment,
+//            property: KProperty<*>
+//        ): T {
+//            return this.binding ?: error("Called before onCreateView or after onDestroyView.")
+//        }
+//
+//        override fun setValue(
+//            thisRef: Fragment,
+//            property: KProperty<*>,
+//            value: T
+//        ) {
+//            this.binding = value
+//        }
+//    }
+//
+//fun Fragment.makeToast(text: String) {
+//    val toast = Toast.makeText(activity, text, Toast.LENGTH_SHORT)
+//
+////    toast.setGravity(Gravity.BOTTOM, 0, 150)
+//    toast.show()
+//}
+//
+//fun View.hideKeyboard() {
+//    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//    imm.hideSoftInputFromWindow(windowToken, 0)
+//}
+//
+//fun View.showKeyboard() {
+//    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//    imm.showSoftInput(this, 0)
+//}
+//
+//fun View.makeVisible() {
+//    this.visibility = View.VISIBLE
+//}
+//
+//fun View.makeGone() {
+//    this.visibility = View.GONE
+//}
+//
+//fun RecyclerView.runLayoutAnimation() {
+//    val context = this.context
+//    val controller: LayoutAnimationController =
+//        AnimationUtils.loadLayoutAnimation(context, R.anim.layout_anmiation_fall_down)
+//    this.layoutAnimation = controller
+//    this.scheduleLayoutAnimation()
+//}
+//
+//fun Context.makeToast(text: String) {
+//    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+//}
+//
+//fun View.makeSnackBar(text: String) {
+//    Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
+//}
+//
+//fun Context.isConnected(): Boolean {
+//    val connectivityManager =
+//        (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetwork
+//    return connectivityManager != null
+//}
+//
+//fun Context.getResourceId(title: String) = resources.getIdentifier(title,"drawable", packageName)
